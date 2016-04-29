c11cd50fa5be6f272b99c25518317a9ed7b968f3
Quentin Bayart <quentin.bayart45430@gmail.com>
2016-04-29 09:23:51 +0200

    Add mockup values to /routes to test

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def upload_file():
 @app.route("/agencies/<int:agency_id>/routes", methods=['GET'])
 def display_routes(agency_id):
     try:
-        return jsonify({'routes': get_routes(agency_id)})
+        return jsonify({'data': get_routes(agency_id)})
     except Exception as e:
         return error(str(e))
 
--- services/display_routes.py
+++ services/display_routes.py
@@ -6,7 +6,10 @@ from services.check_urban import check_urban_category
 
 def get_routes(agency_id):
 	dao = get_dao(agency_id)
+	data = dict()
 	parsedRoutes = list()
+	data['agency'] = "TAN"
+	data['location'] = {'lat': 48.6843900, 'lng': 6.1849600}
 
 	for route in dao.routes(fltr=Route.route_type == Route.TYPE_BUS):
 		print(route)
@@ -16,5 +19,6 @@ def get_routes(agency_id):
 		parsedRoute["category"] = check_urban_category(route.trips)
 		parsedRoutes.append(parsedRoute)
 
-	return parsedRoutes
+	data['routes'] = parsedRoutes
+	return data
 
